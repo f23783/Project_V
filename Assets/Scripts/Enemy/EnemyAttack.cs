e0f59a0e0accea4feee069a5a9870e719844f8a6
f23783 <fidoarda003@gmail.com>
2024-05-07 21:07:10 +0300

    Codes

--- Assets/Scripts/Enemy/EnemyAttack.cs
+++ Assets/Scripts/Enemy/EnemyAttack.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:db38faaa276e8facbad1a0daeee6c8970e4663a7586c3a037631bc4589d51038
-size 1431
+oid sha256:026a24d1baed07239e367f9691dc4ac737319a926ccaab3f401419e2c505071c
+size 398
---
+++ Assets/Scripts/UI/Healt_Bar.cs
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:cfb2613efd255d7c8b70d72ebd26338e824ae59c657178d10908439b2798da6e
+size 267
